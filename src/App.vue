048ae29cd6784651c8833040a2c53cc172c53616
adachi-koichi <1cochan+github@gmail.com>
2022-02-17 18:28:49 +0900

    add files

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <FirstView class="travel-map"/>
+</template>
+
+<script>
+import FirstView from "./components/FirstView.vue";
+
+export default {
+  name: 'App',
+
+  components: {
+    TravelMap,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.travel-map {
+  height: 800px;
+}
+</style>
+
---
+++ src/components/FirstView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+import {SetupContext} from "@vue/runtime-core";
+
+
+export default defineComponent({
+  name: 'TravelMap',
+  components: {
+  },
+  setup: (props, {attrs, slots, emit, expose}: SetupContext) => {
+    return {
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+.google_map_loader {
+  //position: relative;
+  max-width: 100%;
+  //min-height: 200px;
+  aspect-ratio: 1.777;
+  //width: 100px;
+  //height: 100px;
+}
+
+</style>
