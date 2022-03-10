3b006af7427ef8877320ca5905891154548b0f2a
Yuki Hosokawa <y.hosokawa.r@gmail.com>
2022-03-10 19:08:15 +0900

    ストアをインジェクトする

---
+++ src/views/todos.vue
@@ -0,0 +1,21 @@
+<template>
+  Todo一覧！
+</template>
+
+<script lang="ts">
+  import { defineComponent, inject } from 'vue'
+  import { todoKey } from '@/store/todo'
+
+  export default defineComponent({
+    setup() {
+      const todoStore = inject(todoKey)
+      if (!todoStore) {
+        throw new Error('todoStore is not provided')
+      }
+
+      return {
+        todoStore,
+      }
+    },
+  })
+</script>
