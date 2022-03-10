bb865e8ddb33522874eadf58ed3b420c022369ba
Yuki Hosokawa <y.hosokawa.r@gmail.com>
2022-03-10 19:04:00 +0900

    vue-routerを使うための設定を追加

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-const app = createApp(App)
-app.mount('#app')
-
+const app = createApp(App);
+// app.use()でプラグインを注入する
+app.use(router);
+app.mount('#app');
