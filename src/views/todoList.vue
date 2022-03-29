bcd4e2051ca256ae60662c0d78e30667a1c78068
Yuki Hosokawa <y.hosokawa.r@gmail.com>
2022-03-29 19:07:49 +0900

    TodoItemコンポーネントを利用するように修正

--- src/views/todoList.vue
+++ src/views/todoList.vue
@@ -2,50 +2,49 @@
   <h2>Todo一覧！</h2>
   <ul>
     <!-- v-for で配列に要素をマッピングする -->
-    <li
+    <todo-item
       v-for="todo in todoStore.state.todos"
       :key="todo.id"
       :todo="todo"
       @click-title="clickTitle"
       @click-delete="clickDelete"
     >
-      {{ todo.title }}
-    </li>
+    </todo-item>
   </ul>
   <router-link to="/new"> 新規作成 </router-link>
 </template>
 
 <script lang="ts">
-import { defineComponent, inject } from "vue";
-import { todoKey } from "@/store/todo";
-import TodoItem from "@/components/TodoItem.vue";
-import { useRouter } from "vue-router";
+  import { defineComponent, inject } from "vue";
+  import { todoKey } from "@/store/todo";
+  import TodoItem from "@/components/TodoItem.vue";
+  import { useRouter } from "vue-router";
 
-export default defineComponent({
-  components: {
-    TodoItem,
-  },
-  setup() {
-    const todoStore = inject(todoKey);
-    if (!todoStore) {
-      throw new Error("todoStore is not provided");
-    }
+  export default defineComponent({
+    components: {
+      TodoItem,
+    },
+    setup() {
+      const todoStore = inject(todoKey);
+      if (!todoStore) {
+        throw new Error("todoStore is not provided");
+      }
 
-    const router = useRouter();
+      const router = useRouter();
 
-    const clickDelete = (id: number) => {
-      todoStore.deleteTodo(id);
-    };
+      const clickDelete = (id: number) => {
+        todoStore.deleteTodo(id);
+      };
 
-    const clickTitle = (id: number) => {
-      router.push(`/edit/${id}`);
-    };
+      const clickTitle = (id: number) => {
+        router.push(`/edit/${id}`);
+      };
 
-    return {
-      todoStore,
-      clickDelete,
-      clickTitle,
-    };
-  },
-});
+      return {
+        todoStore,
+        clickDelete,
+        clickTitle,
+      };
+    },
+  });
 </script>
